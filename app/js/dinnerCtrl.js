0447e041cc077be8f532a6b19b110e92e3bdc096
Paeron <perjaa@kth.se>
2017-03-29 16:20:08 +0200

    title fix

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -3,17 +3,23 @@
 dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
 
   $scope.numberOfGuests = Dinner.getNumberOfGuests();
-  $scope.starter = Dinner.getCourse("starter");
-  $scope.mainDish = Dinner.getCourse("mainDish");
-  $scope.dessert = Dinner.getCourse("dessert");
+  Dinner.Dish.get({id:Dinner.getCourse("starter")},function(data) {
+    $scope.starter = data.title;
+  });
+  Dinner.Dish.get({id:Dinner.getCourse("mainDish")},function(data) {
+    $scope.mainDish = data.title;
+  });
+  Dinner.Dish.get({id:Dinner.getCourse("dessert")},function(data) {
+    $scope.dessert = data.title;
+  });
   Dinner.getDishPrice("starter",function(price) {
 	$scope.starterPrice = price;
   });
   Dinner.getDishPrice("mainDish",function(price) {
-	$scope.mainDish = price;
+	$scope.mainDishPrice = price;
   });
   Dinner.getDishPrice("dessert",function(price) {
-	$scope.dessert = price;
+	$scope.dessertPrice = price;
   });
   //$scope.mainDishPrice = Dinner.getDishPrice("mainDish");
   //$scope.dessertPrice = Dinner.getDishPrice("dessert");
--- app/js/menuCtrl.js
+++ app/js/menuCtrl.js
@@ -2,18 +2,29 @@
 dinnerPlannerApp.controller('MenuCtrl', function ($scope,Dinner) {
   $scope.people = Dinner.getNumberOfGuests();
 
-  $scope.starter = Dinner.getCourse("starter");
-  $scope.mainDish = Dinner.getCourse("mainDish");
-  $scope.dessert = Dinner.getCourse("dessert");
-  Dinner.getDishPrice("starter",function(price) {
-  $scope.starterPrice = price;
+  //$scope.starter = Dinner.getCourse("starter");
+  //$scope.mainDish = Dinner.getCourse("mainDish");
+  //$scope.dessert = Dinner.getCourse("dessert");
+
+  Dinner.Dish.get({id:Dinner.getCourse("starter")},function(data) {
+    $scope.starter = data.title;
   });
-  Dinner.getDishPrice("mainDish",function(price) {
-  $scope.mainDish = price;
+  Dinner.Dish.get({id:Dinner.getCourse("mainDish")},function(data) {
+    $scope.mainDish = data.title;
   });
-  Dinner.getDishPrice("dessert",function(price) {
-  $scope.dessert = price;
+  Dinner.Dish.get({id:Dinner.getCourse("dessert")},function(data) {
+    $scope.dessert = data.title;
   });
 
-  $scope.totalPrice = Dinner.getTotalMenuPrice();
+  Dinner.getDishPrice("starter",function(price) {
+    $scope.starterPrice = price;
+  });
+  Dinner.getDishPrice("mainDish",function(price) {
+    $scope.mainDishPrice = price;
+  });
+  Dinner.getDishPrice("dessert",function(price) {
+    $scope.dessertPrice = price;
+  });
+
+  $scope.totalPrice = $scope.dessertPrice + $scope.mainDishPrice + $scope.starterPrice;
 });
